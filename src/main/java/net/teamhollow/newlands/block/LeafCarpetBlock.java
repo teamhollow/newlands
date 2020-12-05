1ef13f653dbef52d383d54cb04bfe71c84148d16
Daniel Orr <orrdaniel73@gmail.com>
2020-12-05 19:41:29 +0000

    flowering magnolia leaf carpet property changes
    + breakage on collision with boat
    + replaceable by block

--- src/main/java/net/teamhollow/newlands/block/LeafCarpetBlock.java
+++ src/main/java/net/teamhollow/newlands/block/LeafCarpetBlock.java
@@ -1,10 +1,18 @@
 package net.teamhollow.newlands.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CarpetBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 
 public class LeafCarpetBlock extends CarpetBlock {
@@ -15,5 +23,17 @@ public class LeafCarpetBlock extends CarpetBlock {
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         return world.getBlockState(pos.down()).isOf(Blocks.WATER) || hasTopRim(world, pos.down());
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return Block.createCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 1.0D, 15.0D);
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        super.onEntityCollision(state, world, pos, entity);
+        if (world instanceof ServerWorld && entity instanceof BoatEntity)
+            world.breakBlock(pos, true, entity);
     }
 }
