69590cbef5004cac2609dd1d83686b235b87bd98
Daniel Orr <orrdaniel73@gmail.com>
2020-12-11 20:24:35 +0000

    add hermit crabs to spawn

--- src/main/java/net/teamhollow/newlands/NewLands.java
+++ src/main/java/net/teamhollow/newlands/NewLands.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -56,7 +57,9 @@ public class NewLands implements ModInitializer {
 
     private static void addBiomeModifications() {
         BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.RIVER, Biome.Category.OCEAN), GenerationStep.Feature.VEGETAL_DECORATION, RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, BuiltinRegistries.CONFIGURED_FEATURE.getId(NLConfiguredFeatures.SAND_LAYER)));
+
         BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.BEACH), GenerationStep.Feature.VEGETAL_DECORATION, RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, BuiltinRegistries.CONFIGURED_FEATURE.getId(NLConfiguredFeatures.TREE_TROPICAL_PALM_BEACH)));
+        BiomeModifications.addSpawn(BiomeSelectors.categories(Biome.Category.BEACH), SpawnGroup.CREATURE, NLEntities.HERMIT_CRAB, 5, 2, 5);
     }
 
     public static void log(Level level, String message){
